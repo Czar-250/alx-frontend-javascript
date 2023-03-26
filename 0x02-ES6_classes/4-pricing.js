b08433f3debe30a22a0c617ac3d544ad3200b270
Czar-250 <104949487+Czar-250@users.noreply.github.com>
2023-03-26 22:26:08 +0200

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+/* eslint no-underscore-dangle: 0 */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('amount should be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('currency should be a Currency object');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('amount should be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('conversionRate should be a number');
+    }
+    return (amount * conversionRate);
+  }
+}
